6485acf35ba0537f489bc0f021d6eff7e169e2d7
Stopa <muluan_y@ukr.net>
2022-06-22 16:47:05 +0300

    Worked program with function

--- Source.cpp
+++ Source.cpp
@@ -48,10 +48,8 @@ graph** firstvertex;
 
 int CreateGraph(); // 
 void PrintGraph(int);
-int* PathForAllVertex() {
-
-}
-
+int** PathForAllVertex(int);
+int* PathForGraph(int, int**);
 
 int main() {
 
@@ -59,215 +57,20 @@ int main() {
 
 	int Graphnum = CreateGraph();
 	PrintGraph(Graphnum);
+	int** dArr = PathForAllVertex(Graphnum);
+	int* path = PathForGraph(Graphnum, dArr);
 
-	// V2 Алгоритм Літтла(Алгоритм Дейкстри для кожної вершини):
-	AdjList* pList; // 
-
-	int* Arrres = (int*)malloc(WEIGHT_INT * Graphnum);
-	int** dArr = (int**)malloc(sizeof(int*) * Graphnum);
-	for (int i = 0; i < Graphnum; i++) // <= може видати помилку.
-		dArr[i] = (int*)malloc(WEIGHT_INT * Graphnum);
-
-	for (int i = 0; i < Graphnum; i++) {
-		pList = firstvertex[i]->ListOfVertices;
-		for (int j = 0; j < Graphnum; j++) {
-			dArr[i][j] = 0;
-		}
-		while (pList != NULL) {
-			dArr[i][pList->vertex] = pList->weight;
-			pList = pList->next;
-		}
+	int Weight_Of_The_Path = 0;
+	for (int i = 0; i < 2 * Graphnum; ) {
+		Weight_Of_The_Path += dArr[path[i]][path[i + 1]];
+		i += 2;
 	}
 
-	for (int indGr = 0; indGr < Graphnum; indGr++) {
-		pList = firstvertex[indGr]->ListOfVertices;
+	printf("\n Path - %d \n", Weight_Of_The_Path);
 
-		for (int i = 0; i < Graphnum; i++)
-			Arrres[i] = INT_MAX;
-		while (pList != NULL) {
-			Arrres[pList->vertex] = pList->weight;
-			pList = pList->next;
-		}
-
-		for (int fromv = 1 + indGr; fromv != indGr; fromv++) {
-			if (fromv == Graphnum) {
-				fromv = -1;
-				continue;
-			}
-			for (int tov = 0; tov < Graphnum; tov++) {
-				if (tov == indGr) // 
-					continue;
-
-				if (dArr[fromv][tov] != 0 && (long long int)dArr[fromv][tov] + (long long int)Arrres[fromv] < (long long int)Arrres[tov])
-					Arrres[tov] = dArr[fromv][tov] + Arrres[fromv];
-			}
-		}
-		for (int i = 0; i < Graphnum; i++)
-			dArr[indGr][i] = Arrres[i];
-	}
-
-
-	// !!!!!!!!!!!!!!!!!!!!!--------------------------!!!!!!!!!!!!!!!!!!!!!
-	int minel = INT_MAX;
-
-	for (int i = 0; i < Graphnum; i++) {
-		for (int j = 0; j < Graphnum; j++) {
-			if (dArr[i][j] < minel)
-				minel = dArr[i][j];
-		}
-		for (int j = 0; j < Graphnum; j++) { // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-			if (dArr[i][j] != INT_MAX) // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-				dArr[i][j] -= minel; // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		}
-		minel = INT_MAX;
-	}
-
-	for (int i = 0; i < Graphnum; i++) { // 
-		for (int j = 0; j < Graphnum && i < Graphnum; j++) {
-			if (dArr[j][i] == 0) {
-				i++; j = -1;
-				minel = INT_MAX;
-				continue;
-			}
-			if (dArr[j][i] < minel)
-				minel = dArr[j][i];
-		}
-		for (int j = 0; j < Graphnum && minel != INT_MAX; j++) {
-			if (dArr[j][i] != INT_MAX) // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-				dArr[j][i] -= minel; // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		}
-		minel = INT_MAX;
-	} // 
-
-
-
-	// !!!!!!!!!!!!!!!!!!!!!--------------------------!!!!!!!!!!!!!!!!!!!!!
-	int minel_i = INT_MAX, minel_j = INT_MAX;
-	int ind_i, ind_j;
-	int max_of_sum = 0;
-
-	// !!!!!!!!!!!!!!!!!!!!!--------------------------!!!!!!!!!!!!!!!!!!!!!
-	int* path = (int*)malloc(WEIGHT_INT), * path_copy;
-
-	for (int ind_of_path = 0; ind_of_path < Graphnum - 2; ind_of_path++) { //
-
-		for (int i = 0; i < Graphnum; i++) {
-			for (int j = 0; j < Graphnum; j++) {
-				if (dArr[i][j] == -1)
-					continue;
-				if (dArr[i][j] == 0) {
-					for (int k = 0; k < Graphnum; k++) {
-						if (dArr[i][k] < minel_i && k != j && dArr[i][k] != -1) //
-							minel_i = dArr[i][k];
-					}
-					for (int g = 0; g < Graphnum; g++) {
-						if (dArr[g][j] < minel_j && g != i && dArr[g][j] != -1) //
-							minel_j = dArr[g][j];
-					}
-					if (minel_i + minel_j > max_of_sum) {
-						max_of_sum = minel_i + minel_j;
-						ind_i = i; ind_j = j;
-					}
-				}
-				minel_i = INT_MAX; minel_j = INT_MAX;
-			}
-		}
-		max_of_sum = 0;
-
-		path_copy = (int*)malloc(WEIGHT_INT * 2 * (ind_of_path + 1));
-		for (int i = 0; i < 2 * ind_of_path; i++)
-			path_copy[i] = path[i];
-		path_copy[2 * ind_of_path] = ind_i;
-		path_copy[2 * ind_of_path + 1] = ind_j;
-		free(path);
-		path = path_copy;
-
-
-		for (int j = 0; j < Graphnum; j++)
-			dArr[ind_i][j] = -1;
-		for (int i = 0; i < Graphnum; i++)
-			dArr[i][ind_j] = -1;
-
-		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		int k, j, i;
-		for (int i = 0; i < Graphnum; i++) {
-			for (k = 0; k < Graphnum; k++) {
-				if (dArr[i][k] == -1)
-					continue;
-				k = 0;
-				break;
-			}
-			if (k != 0)
-				continue;
-			for (j = 0; j < Graphnum; j++) {
-				if (dArr[i][j] == INT_MAX) {
-					j = -1;
-					break;
-				}
-			}
-			if (j != -1) {
-				ind_i = i;
-				break;
-			}
-		}
-		for (int j = 0; j < Graphnum; j++) {
-			for (k = 0; k < Graphnum; k++) {
-				if (dArr[k][j] == -1)
-					continue;
-				k = 0;
-				break;
-			}
-			if (k != 0)
-				continue;
-			for (i = 0; i < Graphnum; i++) {
-				if (dArr[i][j] == INT_MAX) {
-					i = -1;
-					break;
-				}
-			}
-			if (i != -1) {
-				ind_j = j;
-				break;
-			}
-		}
-		dArr[ind_i][ind_j] = INT_MAX;
-		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	}
-	for (int i = 0; i < Graphnum; i++) {
-		for (int j = 0; j < Graphnum; j++) {
-			if (dArr[i][j] == -1 || dArr[i][j] == INT_MAX)
-				continue;
-			ind_i = i;
-			ind_j = j;
-			break;
-		}
-	}
-	dArr[ind_i][ind_j] = -1;
-	path_copy = (int*)malloc(WEIGHT_INT * 2 * (Graphnum));
-	for (int i = 0; i < 2 * Graphnum; i++)
-		path_copy[i] = path[i];
-	path_copy[2 * Graphnum - 4] = ind_i;
-	path_copy[2 * Graphnum - 3] = ind_j;
-	free(path);
-	path = path_copy;
-	for (int i = 0; i < Graphnum; i++) {
-		for (int j = 0; j < Graphnum; j++) {
-			if (dArr[i][j] == -1 || dArr[i][j] == INT_MAX)
-				continue;
-			ind_i = i;
-			ind_j = j;
-			break;
-		}
-	}
-	path[2 * Graphnum - 2] = ind_i;
-	path[2 * Graphnum - 1] = ind_j;
-
-
-
-	
 	//
 	puts("\n");
-	for (int i = 0; i < 2 * (Graphnum);) {
+	for (int i = 0; i < 2 * Graphnum;) {
 		printf(" %d - %d \n", path[i], path[i + 1]);
 		i += 2;
 	}
@@ -374,4 +177,205 @@ void PrintGraph(int Graphnum) {
 		}
 	}
 }
+int** PathForAllVertex(int Graphnum) {
+	AdjList* pList; // 
+
+	int* Arrres = (int*)malloc(WEIGHT_INT * Graphnum);
+	int** dArr = (int**)malloc(sizeof(int*) * Graphnum);
+	for (int i = 0; i < Graphnum; i++) // <= може видати помилку.
+		dArr[i] = (int*)malloc(WEIGHT_INT * Graphnum);
+
+	for (int i = 0; i < Graphnum; i++) {
+		pList = firstvertex[i]->ListOfVertices;
+		for (int j = 0; j < Graphnum; j++)
+			dArr[i][j] = 0;
+		while (pList != NULL) {
+			dArr[i][pList->vertex] = pList->weight;
+			pList = pList->next;
+		}
+	}
+
+	for (int indGr = 0; indGr < Graphnum; indGr++) {
+		pList = firstvertex[indGr]->ListOfVertices;
+
+		for (int i = 0; i < Graphnum; i++)
+			Arrres[i] = INT_MAX;
+		while (pList != NULL) {
+			Arrres[pList->vertex] = pList->weight;
+			pList = pList->next;
+		}
+
+		for (int fromv = 1 + indGr; fromv != indGr; fromv++) {
+			if (fromv == Graphnum) {
+				fromv = -1;
+				continue;
+			}
+			for (int tov = 0; tov < Graphnum; tov++) {
+				if (tov == indGr) // 
+					continue;
+
+				if (dArr[fromv][tov] != 0 && (long long int)dArr[fromv][tov] + (long long int)Arrres[fromv] < (long long int)Arrres[tov])
+					Arrres[tov] = dArr[fromv][tov] + Arrres[fromv];
+			}
+		}
+		for (int i = 0; i < Graphnum; i++)
+			dArr[indGr][i] = Arrres[i];
+	}
+
+	free(Arrres);
+
+	return dArr;
+}
+int* PathForGraph(int Graphnum, int** dArr) {
+	int minel = INT_MAX;
+	int** dArr_copy = (int**)malloc(sizeof(int*) * Graphnum);
+	for (int i = 0; i < Graphnum; i++) {
+		dArr_copy[i] = (int*)malloc(WEIGHT_INT * Graphnum);
+		for (int j = 0; j < Graphnum; j++)
+			dArr_copy[i][j] = dArr[i][j];
+	}
+
+	for (int i = 0; i < Graphnum; i++) {
+		for (int j = 0; j < Graphnum; j++) {
+			if (dArr_copy[i][j] < minel)
+				minel = dArr_copy[i][j];
+		}
+		for (int j = 0; j < Graphnum; j++) {
+			if (dArr_copy[i][j] != INT_MAX)
+				dArr_copy[i][j] -= minel;
+		}
+		minel = INT_MAX;
+	}
+	for (int i = 0; i < Graphnum; i++) {
+		for (int j = 0; j < Graphnum && i < Graphnum; j++) {
+			if (dArr_copy[j][i] == 0) {
+				i++; j = -1;
+				minel = INT_MAX;
+				continue;
+			}
+			if (dArr_copy[j][i] < minel)
+				minel = dArr_copy[j][i];
+		}
+		for (int j = 0; j < Graphnum && minel != INT_MAX; j++) {
+			if (dArr_copy[j][i] != INT_MAX)
+				dArr_copy[j][i] -= minel;
+		}
+		minel = INT_MAX;
+	}
+
+	int minel_i = INT_MAX, minel_j = INT_MAX;
+	int ind_i, ind_j;
+	int max_of_sum = 0;
+	int* path = (int*)malloc(WEIGHT_INT * 2 * (Graphnum));
+	int k, j, i;
+
+	for (int ind_of_path = 0; ind_of_path < Graphnum - 2; ind_of_path++) {
+		for (int i = 0; i < Graphnum; i++) {
+			for (int j = 0; j < Graphnum; j++) {
+				if (dArr_copy[i][j] == -1)
+					continue;
+				if (dArr_copy[i][j] == 0) {
+					for (int k = 0; k < Graphnum; k++) {
+						if (dArr_copy[i][k] < minel_i && k != j && dArr_copy[i][k] != -1)
+							minel_i = dArr_copy[i][k];
+					}
+					for (int g = 0; g < Graphnum; g++) {
+						if (dArr_copy[g][j] < minel_j && g != i && dArr_copy[g][j] != -1)
+							minel_j = dArr_copy[g][j];
+					}
+					if (minel_i + minel_j > max_of_sum) {
+						max_of_sum = minel_i + minel_j;
+						ind_i = i; ind_j = j;
+					}
+				}
+				minel_i = INT_MAX; minel_j = INT_MAX;
+			}
+		}
+		max_of_sum = 0;
+
+		path[2 * ind_of_path] = ind_i;
+		path[2 * ind_of_path + 1] = ind_j;
+
+		for (int j = 0; j < Graphnum; j++)
+			dArr_copy[ind_i][j] = -1;
+		for (int i = 0; i < Graphnum; i++)
+			dArr_copy[i][ind_j] = -1;
+
+		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		// !!!
+		for (int i = 0; i < Graphnum; i++) {
+			for (k = 0; k < Graphnum; k++) {
+				if (dArr_copy[i][k] == -1)
+					continue;
+				k = 0;
+				break;
+			}
+			if (k != 0)
+				continue;
+			for (j = 0; j < Graphnum; j++) {
+				if (dArr_copy[i][j] == INT_MAX) {
+					j = -1;
+					break;
+				}
+			}
+			if (j != -1) {
+				ind_i = i;
+				break;
+			}
+		}
+		for (int j = 0; j < Graphnum; j++) {
+			for (k = 0; k < Graphnum; k++) {
+				if (dArr_copy[k][j] == -1)
+					continue;
+				k = 0;
+				break;
+			}
+			if (k != 0)
+				continue;
+			for (i = 0; i < Graphnum; i++) {
+				if (dArr_copy[i][j] == INT_MAX) {
+					i = -1;
+					break;
+				}
+			}
+			if (i != -1) {
+				ind_j = j;
+				break;
+			}
+		}
+		dArr_copy[ind_i][ind_j] = INT_MAX;
+		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	}
+	for (int i = 0; i < Graphnum; i++) {
+		for (int j = 0; j < Graphnum; j++) {
+			if (dArr_copy[i][j] == -1 || dArr_copy[i][j] == INT_MAX)
+				continue;
+			ind_i = i;
+			ind_j = j;
+			break;
+		}
+	}
+	dArr_copy[ind_i][ind_j] = -1;
+	path[2 * Graphnum - 4] = ind_i; // 
+	path[2 * Graphnum - 3] = ind_j; //
+	for (int i = 0; i < Graphnum; i++) {
+		for (int j = 0; j < Graphnum; j++) {
+			if (dArr_copy[i][j] == -1 || dArr_copy[i][j] == INT_MAX)
+				continue;
+			ind_i = i;
+			ind_j = j;
+			break;
+		}
+	}
+	path[2 * Graphnum - 2] = ind_i;
+	path[2 * Graphnum - 1] = ind_j;
+	for (int i = 0; i < Graphnum; i++)
+		free(dArr_copy[i]);
+	free(dArr_copy);
+
+	return path;
+}
+
+
+
 
